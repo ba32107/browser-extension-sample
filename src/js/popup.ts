338d259729e26edc9a1acd4c8e1a4ed0cea494ce
Balazs Gyurak <ba32107@gmail.com>
2022-01-24 21:26:51 +0000

    Add settings skeleton (#6)

--- src/js/popup.ts
+++ src/js/popup.ts
@@ -1,15 +1,32 @@
 import * as browser from "webextension-polyfill"
 import logger from "./logger"
+import { getSettings, ISettings, saveSettings } from "./settings"
 
-const myButton = document.getElementById("myButton") as HTMLButtonElement
+const option1Checkbox = document.getElementById("option1Checkbox") as HTMLInputElement
+const option2TextField = document.getElementById("option2TextField") as HTMLInputElement
+const saveButton = document.getElementById("saveButton") as HTMLButtonElement
 
-myButton.onclick = function () {
-    logger.info("Button clicked")
+window.onload = async function () {
+    const settings = await getSettings()
+
+    option1Checkbox.checked = settings.option1
+    option2TextField.value = settings.option2
+}
+
+saveButton.onclick = async function () {
+    logger.info("Save button clicked")
+
+    const newSettings: ISettings = {
+        option1: option1Checkbox.checked,
+        option2: option2TextField.value
+    }
+
+    await saveSettings(newSettings)
 
     browser.notifications.create(undefined, {
         type: "basic",
         iconUrl: "icon32.png",
         title: "Browser extension template",
-        message: "Hello World!"
+        message: "Settings saved"
     })
 }

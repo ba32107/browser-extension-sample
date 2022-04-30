ff9a1ea51cbc8b7fecc80e448db60df7a80c45d2
Balazs Gyurak <ba32107@gmail.com>
2022-04-30 08:59:00 +0100

    Minor fixes

--- src/js/util/env.ts
+++ src/js/util/env.ts
@@ -1,19 +1,21 @@
 import * as browser from 'webextension-polyfill'
 
 declare const __PRODUCTION__: boolean
-const IsProduction = __PRODUCTION__
-export default IsProduction
+export const IsProduction = __PRODUCTION__
 
-export async function isDevEnv() {
-    const extensionInfo = await browser.management.getSelf()
-    return extensionInfo.installType === 'development'
-}
-
-export async function isFirefox() {
+export async function isFirefox(): Promise<boolean>  {
     if (browser.runtime.getBrowserInfo) {
         const browserInfo = await browser.runtime.getBrowserInfo()
         return browserInfo.name.toLowerCase().includes('firefox')
     }
 
     return false
+}
+
+export async function getOperatingSystem(): Promise<string>  {
+    return (await browser.runtime.getPlatformInfo()).os
+}
+
+export function getVersion(): string {
+    return browser.runtime.getManifest().version
 }
